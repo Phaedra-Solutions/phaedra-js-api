50bf07a0ac98de60afaa49a0a8f8aa313cc5367f
musmanfaisal <m.usmanfaisal@phaedrasolutions.com>
2021-06-15 18:54:30 +0500

    removed dist from gitignore

---
+++ dist/base-apis.js
@@ -0,0 +1,133 @@
+"use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BaseApis = void 0;
+var axios_1 = __importDefault(require("axios"));
+var BaseApis = /** @class */ (function () {
+    function BaseApis() {
+    }
+    BaseApis.init = function (baseUrl, logging, options) {
+        if (options === void 0) { options = {}; }
+        axios_1.default.defaults.baseURL = baseUrl;
+        BaseApis.logging = logging || false;
+        BaseApis.options = {
+            auhtorizationPrefix: options.auhtorizationPrefix !== undefined ?
+                options.auhtorizationPrefix
+                :
+                    BaseApis.options.auhtorizationPrefix,
+            authorizationKey: options.authorizationKey !== undefined ?
+                options.authorizationKey
+                :
+                    BaseApis.options.authorizationKey
+        };
+    };
+    BaseApis.prototype.get = function (route, authToken, type, headers) {
+        if (headers === void 0) { headers = {}; }
+        var _headers = __assign({}, headers);
+        if (authToken)
+            _headers[BaseApis.options.authorizationKey] = BaseApis.options.auhtorizationPrefix + authToken;
+        BaseApis.logging && console.log('GET API INVOKED', route);
+        return type === 'get' ?
+            axios_1.default({
+                method: "get",
+                url: route,
+                headers: _headers
+            })
+            :
+                axios_1.default.get(route, {
+                    headers: _headers
+                });
+    };
+    BaseApis.prototype.post = function (route, data, authToken, type, headers) {
+        if (headers === void 0) { headers = {}; }
+        var _headers = __assign({}, headers);
+        if (authToken)
+            _headers[BaseApis.options.authorizationKey] = BaseApis.options.auhtorizationPrefix + authToken;
+        BaseApis.logging && console.log('POST API INVOKED', route, "DATA", data);
+        return type === 'post' ?
+            axios_1.default({
+                method: "post",
+                url: route,
+                data: data,
+                headers: _headers
+            })
+            :
+                axios_1.default.post(route, data, {
+                    headers: _headers
+                });
+    };
+    BaseApis.prototype.put = function (route, data, authToken, type, headers) {
+        if (headers === void 0) { headers = {}; }
+        var _headers = __assign({}, headers);
+        if (authToken)
+            _headers[BaseApis.options.authorizationKey] = BaseApis.options.auhtorizationPrefix + authToken;
+        BaseApis.logging && console.log('PUT API INVOKED', route, "DATA", data);
+        return type === 'put' ?
+            axios_1.default({
+                method: "put",
+                url: route,
+                data: data,
+                headers: _headers
+            })
+            :
+                axios_1.default.put(route, data, {
+                    headers: _headers
+                });
+    };
+    BaseApis.prototype.patch = function (route, data, authToken, type, headers) {
+        if (headers === void 0) { headers = {}; }
+        var _headers = __assign({}, headers);
+        if (authToken)
+            _headers[BaseApis.options.authorizationKey] = BaseApis.options.auhtorizationPrefix + authToken;
+        BaseApis.logging && console.log('PATCH API INVOKED', route, "DATA", data);
+        return type === 'patch' ?
+            axios_1.default({
+                method: 'patch',
+                url: route,
+                data: data,
+                headers: _headers
+            })
+            :
+                axios_1.default.patch(route, data, {
+                    headers: _headers
+                });
+    };
+    BaseApis.prototype.delete = function (route, data, authToken, type, headers) {
+        if (headers === void 0) { headers = {}; }
+        var _headers = __assign({}, headers);
+        if (authToken)
+            _headers[BaseApis.options.authorizationKey] = BaseApis.options.auhtorizationPrefix + authToken;
+        BaseApis.logging && console.log('DELETE API INVOKED', route, "DATA", data);
+        return type === 'delete' ?
+            axios_1.default({
+                method: "delete",
+                url: route,
+                data: data,
+                headers: _headers
+            })
+            :
+                axios_1.default.delete(route, {
+                    headers: _headers,
+                    data: data
+                });
+    };
+    BaseApis.options = {
+        authorizationKey: "Authorization",
+        auhtorizationPrefix: "Bearer ",
+    };
+    return BaseApis;
+}());
+exports.BaseApis = BaseApis;
---
+++ dist/enpoint.js
@@ -0,0 +1,97 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Endpoint = void 0;
+var base_apis_1 = require("./base-apis");
+var Endpoint = /** @class */ (function (_super) {
+    __extends(Endpoint, _super);
+    function Endpoint(path, type, isGuarded) {
+        var _this = _super.call(this) || this;
+        _this.call = function (data, additionalData, authToken) {
+            return typeof additionalData === "string" ?
+                this._call(data, undefined, additionalData)
+                :
+                    this._call(data, additionalData, authToken);
+        };
+        _this.path = path;
+        _this.type = type;
+        _this.isGuarded = isGuarded;
+        return _this;
+    }
+    Endpoint.prototype._call = function (data, options, authToken) {
+        if (data === void 0) { data = {}; }
+        if (this.isGuarded && !authToken)
+            throw new Error("AUTH_TOKEN_REQUIRED");
+        switch (this.type) {
+            case "GET":
+            case "get":
+                {
+                    var _path = this.appendDataToPath(data, this.path, options === null || options === void 0 ? void 0 : options.type)._path;
+                    return this.get(_path, authToken, this.type, options === null || options === void 0 ? void 0 : options.headers);
+                }
+            case "POST":
+            case "post":
+                return this.post(this.path, data, authToken, this.type, options === null || options === void 0 ? void 0 : options.headers);
+            case "PUT":
+            case "put":
+                {
+                    if ((options === null || options === void 0 ? void 0 : options.type) === 'query-params')
+                        throw new Error("QUERY_PARAMS_NOT SUPPORTED");
+                    var _a = this.appendDataToPath(data, this.path, options === null || options === void 0 ? void 0 : options.type), _path = _a._path, _data = _a._data;
+                    return this.put(_path, _data, authToken, this.type, options === null || options === void 0 ? void 0 : options.headers);
+                }
+            case "PATCH":
+            case "patch":
+                {
+                    if ((options === null || options === void 0 ? void 0 : options.type) === 'query-params')
+                        throw new Error("QUERY_PARAMS_NOT SUPPORTED");
+                    var _b = this.appendDataToPath(data, this.path, options === null || options === void 0 ? void 0 : options.type), _path = _b._path, _data = _b._data;
+                    return this.patch(_path, _data, authToken, this.type, options === null || options === void 0 ? void 0 : options.headers);
+                }
+            case "DELETE":
+            case "delete":
+                {
+                    var _c = this.appendDataToPath(data, this.path, options === null || options === void 0 ? void 0 : options.type), _path = _c._path, _data = _c._data;
+                    return this.delete(_path, (options === null || options === void 0 ? void 0 : options.type) === 'query-params' ? {} : _data, authToken, this.type, options === null || options === void 0 ? void 0 : options.headers);
+                }
+            default:
+                throw new Error("INVALID_CONFIGURATION");
+        }
+    };
+    Endpoint.prototype.appendDataToPath = function (data, path, type) {
+        var _data = {};
+        var _path = path;
+        if (type === 'query-params') {
+            var queryParams = "?" + Object.keys(data).map(function (key) { return key + "=" + data[key]; }).join("&");
+            _path = _path + queryParams;
+        }
+        else {
+            Object.keys(data).forEach(function (key) {
+                if (_path.includes(":" + key)) {
+                    _path = _path.split(":" + key).join(data[key]);
+                    data[key] = undefined;
+                }
+                else {
+                    _data[key] = data[key];
+                }
+            });
+        }
+        return { _data: _data, _path: _path };
+    };
+    return Endpoint;
+}(base_apis_1.BaseApis));
+exports.Endpoint = Endpoint;
